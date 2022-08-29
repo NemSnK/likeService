fc3db2e86c6f4a319ef072879dec58b346aebb96
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-08-29 16:54:21 +0600

    fix

--- src/main/java/kata/academy/eurekalikeservice/rest/inner/PostLikeInternalRestController.java
+++ src/main/java/kata/academy/eurekalikeservice/rest/inner/PostLikeInternalRestController.java
@@ -2,24 +2,26 @@ package kata.academy.eurekalikeservice.rest.inner;
 
 import kata.academy.eurekalikeservice.service.PostLikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Positive;
 
-
 @RequiredArgsConstructor
 @Validated
 @RestController
-@RequestMapping("/api/internal/v1/likes")
-public class LikePostInternalRestController {
+@RequestMapping("/api/internal/v1/posts")
+public class PostLikeInternalRestController {
+
     private final PostLikeService postLikeService;
 
-    @GetMapping("/{postId}/delete")
-    public void deleteAllByPostId(@PathVariable @Positive Long postId) {
-        postLikeService.deleteAllByPostId(postId);
+    @DeleteMapping("/{postId}/post-likes")
+    public ResponseEntity<Void> deleteByPostId(@PathVariable @Positive Long postId) {
+        postLikeService.deleteByPostId(postId);
+        return ResponseEntity.ok().build();
     }
 }
