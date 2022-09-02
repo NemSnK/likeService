14fd38420a43055e7b4b7f0c65a593dabbd1d50e
Zhassulan Kaitanov <kaitanov.work@gmail.com>
2022-09-03 01:42:44 +0600

    BACK17 add deleting in batch

--- src/main/java/kata/academy/eurekalikeservice/repository/CommentLikeRepository.java
+++ src/main/java/kata/academy/eurekalikeservice/repository/CommentLikeRepository.java
@@ -10,11 +10,17 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> 
 
     boolean existsByIdAndCommentIdAndUserId(Long commentLikeId, Long commentId, Long userId);
 
-
     @Query("""
             SELECT cl.id
             FROM CommentLike cl
             WHERE cl.commentId = :commentId
                                 """)
     List<Long> findAllIdsByCommentId(Long commentId);
+
+    @Query("""
+            SELECT cl.id
+            FROM CommentLike cl
+            WHERE cl.commentId IN :commentIds
+                                """)
+    List<Long> findAllIdsByCommentIds(List<Long> commentIds);
 }
