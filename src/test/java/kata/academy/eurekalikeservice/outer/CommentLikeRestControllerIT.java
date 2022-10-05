8cc6fbca5cbdc72e5511a35438c2d167ebd4cedb
Alexander Nemkov <nemsnknet@yandex.ru>
2022-10-05 11:49:02 +0300

    BACK38 add variabls for each methods

--- src/test/java/kata/academy/eurekalikeservice/outer/CommentLikeRestControllerIT.java
+++ src/test/java/kata/academy/eurekalikeservice/outer/CommentLikeRestControllerIT.java
@@ -28,26 +28,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Autowired
     private ContentServiceFeignClient contentServiceFeignClient;
-    private Long COMMENT_ID = 1L;
-    private Long USER_ID = 1L;
-    private Long COMMENT_LIKE_ID = 1L;
 
     @Test
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/addCommentLike_SuccessfulTest/After.sql")
     public void addCommentLike_SuccessfulTest() throws Exception {
-        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(COMMENT_ID);
+        Long commentId = 1L;
+        Long userId = 1L;
+        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(commentId);
         CommentLikePersistRequestDto dto = new CommentLikePersistRequestDto(true);
-        mockMvc.perform(post("/api/v1/comments/{commentId}/comment-likes", COMMENT_ID)
-                        .param("userId", USER_ID.toString())
+        mockMvc.perform(post("/api/v1/comments/{commentId}/comment-likes", commentId)
+                        .param("userId", userId.toString())
                         .content(objectMapper.writeValueAsString(dto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(true)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(200)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.id", Is.is(1)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.commentId", Is.is(COMMENT_ID.intValue())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId", Is.is(USER_ID.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.commentId", Is.is(commentId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId", Is.is(userId.intValue())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.positive", Is.is(dto.positive())));
         assertTrue(entityManager.createQuery(
                         """
@@ -57,8 +56,8 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                                 AND cl.userId = :userId
                                 AND cl.positive = :positive
                                 """, Boolean.class)
-                .setParameter("commentId", COMMENT_ID)
-                .setParameter("userId", USER_ID)
+                .setParameter("commentId", commentId)
+                .setParameter("userId", userId)
                 .setParameter("positive", dto.positive())
                 .getSingleResult());
     }
@@ -69,10 +68,12 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/addCommentLike_CommentLikeExistsTest/After.sql")
     public void addCommentLike_CommentLikeExistsTest() throws Exception {
-        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(COMMENT_ID);
+        Long commentId = 1L;
+        Long userId = 1L;
+        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(commentId);
         CommentLikePersistRequestDto dto = new CommentLikePersistRequestDto(true);
-        mockMvc.perform(post("/api/v1/comments/{commentId}/comment-likes", COMMENT_ID)
-                        .param("userId", USER_ID.toString())
+        mockMvc.perform(post("/api/v1/comments/{commentId}/comment-likes", commentId)
+                        .param("userId", userId.toString())
                         .content(objectMapper.writeValueAsString(dto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -80,7 +81,7 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(400)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.error", Is.is(
                         String.format("Пользователь userId %d уже поставил лайк на комментарий commentId %d",
-                                COMMENT_ID, USER_ID))));
+                                commentId, userId))));
     }
 
     @Test
@@ -89,9 +90,12 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/deleteCommentLike_SuccessfulTest/After.sql")
     public void deleteCommentLike_SuccessfulTest() throws Exception {
+        Long commentId = 1L;
+        Long commentLikeId = 1L;
+        Long userId = 1L;
         mockMvc.perform(delete("/api/v1/comments/{commentId}/comment-likes/{commentLikeId}",
-                        COMMENT_ID, COMMENT_LIKE_ID)
-                        .param("userId", USER_ID.toString())
+                        commentId, commentLikeId)
+                        .param("userId", userId.toString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(true)))
@@ -104,8 +108,8 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                                 AND cl.userId = :userId
                                 AND cl.positive = :positive
                                 """, Boolean.class)
-                .setParameter("commentId", COMMENT_ID)
-                .setParameter("userId", USER_ID)
+                .setParameter("commentId", commentId)
+                .setParameter("userId", userId)
                 .setParameter("positive", Boolean.TRUE)
                 .getSingleResult());
     }
@@ -114,16 +118,19 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/deleteCommentLike_NotExistsTest/After.sql")
     public void deleteCommentLike_NotExistsTest() throws Exception {
+        Long commentId = 1L;
+        Long commentLikeId = 1L;
+        Long userId = 1L;
         mockMvc.perform(delete("/api/v1/comments/{commentId}/comment-likes/{commentLikeId}",
-                        COMMENT_ID, COMMENT_LIKE_ID)
-                        .param("userId", USER_ID.toString())
+                        commentId, commentLikeId)
+                        .param("userId", userId.toString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(false)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(400)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.error", Is.is(
                         String.format("Лайк комментария с commentLikeId %d, commentId %d, userId %d нет в базе данных",
-                                COMMENT_LIKE_ID, COMMENT_ID, USER_ID))));
+                                commentLikeId, commentId, userId))));
     }
 
     @Test
@@ -132,8 +139,10 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/getCommentLikeCount_SuccessfulTest/After.sql")
     public void getCommentLikeCount_SuccessfulTest() throws Exception {
+        Long commentId = 1L;
+        Long userId = 1L;
         Boolean positive = true;
-        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(COMMENT_ID);
+        doReturn(ResponseEntity.ok(Boolean.TRUE)).when(contentServiceFeignClient).existsByCommentId(commentId);
         Long count = entityManager.createQuery(
                         """
                                 SELECT COUNT (cl.id)
@@ -141,11 +150,11 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                                 WHERE cl.commentId= :commentId
                                 AND cl.positive = :positive
                                 """, Long.class)
-                .setParameter("commentId", COMMENT_ID)
+                .setParameter("commentId", commentId)
                 .setParameter("positive", positive)
                 .getSingleResult();
-        mockMvc.perform(get("/api/v1/comments/{commentId}/comment-likes/count", COMMENT_ID)
-                        .param("userId", USER_ID.toString())
+        mockMvc.perform(get("/api/v1/comments/{commentId}/comment-likes/count", commentId)
+                        .param("userId", userId.toString())
                         .param("positive", positive.toString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -161,18 +170,21 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/updateCommentLike_SuccessfulTest/After.sql")
     public void updateCommentLike_SuccessfulTest() throws Exception {
+        Long commentId = 1L;
+        Long commentLikeId = 1L;
+        Long userId = 1L;
         CommentLikePersistRequestDto dto = new CommentLikePersistRequestDto(false);
         mockMvc.perform(put("/api/v1/comments/{commentId}/comment-likes/{commentLikeId}",
-                        COMMENT_ID, COMMENT_LIKE_ID)
-                        .param("userId", USER_ID.toString())
+                        commentId, commentLikeId)
+                        .param("userId", userId.toString())
                         .content(objectMapper.writeValueAsString(dto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(true)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(200)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.id", Is.is(1)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.commentId", Is.is(COMMENT_ID.intValue())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId", Is.is(USER_ID.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.commentId", Is.is(commentId.intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.userId", Is.is(userId.intValue())))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.positive", Is.is(dto.positive())));
         assertTrue(entityManager.createQuery(
                         """
@@ -182,8 +194,8 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                                 AND cl.userId = :userId
                                 AND cl.positive = :positive
                                 """, Boolean.class)
-                .setParameter("commentId", COMMENT_ID)
-                .setParameter("userId", USER_ID)
+                .setParameter("commentId", commentId)
+                .setParameter("userId", userId)
                 .setParameter("positive", dto.positive())
                 .getSingleResult());
     }
@@ -192,10 +204,13 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
     @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
             value = "/scripts/outer/CommentLikeRestController/updateCommentLike_NotExistsTest/After.sql")
     public void updateCommentLike_NotExistsTest() throws Exception {
+        Long commentId = 1L;
+        Long commentLikeId = 1L;
+        Long userId = 1L;
         CommentLikePersistRequestDto dto = new CommentLikePersistRequestDto(false);
         mockMvc.perform(put("/api/v1/comments/{commentId}/comment-likes/{commentLikeId}",
-                        COMMENT_ID, COMMENT_LIKE_ID)
-                        .param("userId", USER_ID.toString())
+                        commentId, commentLikeId)
+                        .param("userId", userId.toString())
                         .content(objectMapper.writeValueAsString(dto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -203,6 +218,6 @@ public class CommentLikeRestControllerIT extends SpringSimpleContextTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(400)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.error", Is.is(
                         String.format("Лайк комментария с commentLikeId %d, commentId %d, userId %d нет в базе данных",
-                                COMMENT_LIKE_ID, COMMENT_ID, USER_ID))));
+                                commentLikeId, commentId, userId))));
     }
 }
